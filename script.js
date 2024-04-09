49b6a473f8f45b85ceb9bef8a15f5998ee60c1b7
Julie Philippe <philippejuliepro@gmail.com>
2024-04-09 15:55:08 +0200

    exercice

--- script.js
+++ script.js
@@ -104,3 +104,46 @@ buttonCategories.forEach(buttonCategory => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.getElementById('login-form');
+  
+  loginForm.addEventListener(function (event) {
+      event.preventDefault(); // Empêcher la soumission du formulaire par défaut
+
+      // Récupérer les valeurs des champs email et mot de passe
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      // Construire l'objet de données à envoyer au serveur
+      const data = {
+          email: email,
+          password: password
+      };
+
+      // Envoi des données au serveur
+      fetch('http://localhost:5678/api/users/login', {
+          method: 'POST',
+          body: JSON.stringify(data)
+      })
+      .then(response => {
+          if (response.ok) {
+              return response.json();
+          } else {
+              throw new Error('Erreur lors de la soumission des données');
+          }
+      })
+      .then(data => {
+          // Traitement de la réponse du serveur si nécessaire
+          console.log('Réponse du serveur:', data);
+      })
+      .catch(error => {
+          console.error('Erreur:', error);
+      });
+
+      // Effacer les champs du formulaire après la soumission
+      document.getElementById('email').value = '';
+      document.getElementById('password').value = '';
+  });
+});
+
